c0f4b8ef56655d043b9a56e672faa9e624390b1c
senbeyxd <nicci250404@gmail.com>
2023-06-12 23:51:58 +0200

    lil hover effect.

--- js/main.js
+++ js/main.js
@@ -40,13 +40,17 @@ exampleUser.chats = [exampleChat];
 exampleChat.users = [exampleUser, exampleUser2];
 exampleChat.unreadChats = [exampleChat];
 
-// declaration of the currently logged in user. will be replaced later with the real user.
+// declaration of the user. has to be outside of a block to be global.
 
 const user = exampleUser;
 
 // after this line, the main script starts.
 
 document.addEventListener("DOMContentLoaded", function () {
+  // declaration of everything i need.
+
+  const statBoxes = document.querySelectorAll(".box");
+
   // switches between the friendlist and the chat overview, based on a given parameter on page load => see ./index.html line 10-11.
   // i have passed this function the parameter true, to tell the function that it has been called on page load, because i didnt
   // want to type most of the code 2 times. => see ./js/functions.js line 11.
@@ -74,6 +78,22 @@ document.addEventListener("DOMContentLoaded", function () {
     { id: "onlineUserCount", value: 123 },
     { id: "pageViewCount", value: 123 },
   ].forEach((i) => addStats(i));
+
+  // the hover animation for the boxes.
+
+  statBoxes.forEach((i) => {
+    i.addEventListener("mousemove", function (e) {
+      const rect = i.getBoundingClientRect();
+
+      i.style.transform = `perspective(1000px) rotateX(${
+        (e.clientY - (rect.top + rect.height / 2)) / 10
+      }deg) rotateY(${(e.clientX - (rect.left + rect.width / 2)) / 10}deg)`;
+    });
+
+    i.addEventListener("mouseout", () => {
+      i.style.transform = "none";
+    });
+  });
 });
 
 // as you recommended me one day, i put all the functions in a seperate file to make it easier to view and understand the code.

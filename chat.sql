af9ac22f5875b19ec5a9bdcfa07abce335ec075a
senbey <nicci250404@gmail.com>
2023-06-19 13:47:16 +0200

    temporary db

--- chat.sql
+++ chat.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.4.28-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
+-- Server-Version:               10.4.28-MariaDB - mariadb.org binary distribution
+-- Server-Betriebssystem:        Win64
 -- HeidiSQL Version:             12.5.0.6677
 -- --------------------------------------------------------
 
@@ -15,11 +15,11 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dumping database structure for chat
+-- Exportiere Datenbank-Struktur für chat
 CREATE DATABASE IF NOT EXISTS `chat` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `chat`;
 
--- Dumping structure for table chat.changelog
+-- Exportiere Struktur von Tabelle chat.changelog
 CREATE TABLE IF NOT EXISTS `changelog` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
@@ -28,29 +28,35 @@ CREATE TABLE IF NOT EXISTS `changelog` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Exportiere Daten aus Tabelle chat.changelog: ~0 rows (ungefähr)
 
--- Dumping structure for table chat.chats
+-- Exportiere Struktur von Tabelle chat.chats
 CREATE TABLE IF NOT EXISTS `chats` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id_start` int(11) DEFAULT NULL,
   `user_id_second` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Exportiere Daten aus Tabelle chat.chats: ~2 rows (ungefähr)
+REPLACE INTO `chats` (`id`, `user_id_start`, `user_id_second`) VALUES
+	(1, 66, 67),
+	(2, 67, 68);
 
--- Dumping structure for table chat.friends
+-- Exportiere Struktur von Tabelle chat.friends
 CREATE TABLE IF NOT EXISTS `friends` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id_sender` int(11) DEFAULT NULL,
   `user_id_accepter` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Exportiere Daten aus Tabelle chat.friends: ~2 rows (ungefähr)
+REPLACE INTO `friends` (`id`, `user_id_sender`, `user_id_accepter`) VALUES
+	(1, 67, 66),
+	(2, 68, 67);
 
--- Dumping structure for table chat.friends_requests
+-- Exportiere Struktur von Tabelle chat.friends_requests
 CREATE TABLE IF NOT EXISTS `friends_requests` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` enum('pending','seen','accepted') NOT NULL,
@@ -59,9 +65,9 @@ CREATE TABLE IF NOT EXISTS `friends_requests` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Exportiere Daten aus Tabelle chat.friends_requests: ~0 rows (ungefähr)
 
--- Dumping structure for table chat.messages
+-- Exportiere Struktur von Tabelle chat.messages
 CREATE TABLE IF NOT EXISTS `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `chat_id` int(11) NOT NULL,
@@ -69,22 +75,33 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `send_by_user_id` int(11) DEFAULT NULL,
   `send_at` timestamp NULL DEFAULT NULL,
   `seen` enum('yes','no') DEFAULT 'no',
+  `deleted` enum('Y','N') DEFAULT 'N',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Exportiere Daten aus Tabelle chat.messages: ~4 rows (ungefähr)
+REPLACE INTO `messages` (`id`, `chat_id`, `message`, `send_by_user_id`, `send_at`, `seen`, `deleted`) VALUES
+	(1, 1, 'test', 66, '2023-06-19 10:01:33', 'yes', 'N'),
+	(2, 1, 'test123', 67, '2023-06-19 10:01:53', 'yes', 'N'),
+	(3, 2, 'testing123', 68, '2023-06-19 10:07:44', 'yes', 'N'),
+	(4, 2, 'a', 67, '2023-06-19 10:10:33', 'no', 'N');
 
--- Dumping structure for table chat.users
+-- Exportiere Struktur von Tabelle chat.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` char(50) DEFAULT NULL,
   `password` char(50) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
+  `status` enum('Online','Offline','Abwesend') DEFAULT 'Online',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Exportiere Daten aus Tabelle chat.users: ~3 rows (ungefähr)
+REPLACE INTO `users` (`id`, `username`, `password`, `created_at`, `status`) VALUES
+	(66, 'senbey', 'passwort123', '2023-06-19 05:58:21', 'Online'),
+	(67, 'czubik', 'test1234', '2023-06-19 07:36:16', 'Online'),
+	(68, 'admin', 'password', '2023-06-19 09:16:42', 'Online');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
